5cb5de0377ef11416f073fc7f011224ce50c9c02
Simon Remøy <simon.remoy@gmail.com>
2010-12-10 14:09:52 +0100

    added power

---
+++ calculator/src/main/java/no/ntnu/Power.java
@@ -0,0 +1,11 @@
+package no.ntnu;
+
+public class Power {
+	public static int power(int base, int power){
+		int result = base;
+		for(int i = 1; i < power; i++){
+			result = Multiplication.multiply(result, base);
+		}
+		return result;
+	}
+}
