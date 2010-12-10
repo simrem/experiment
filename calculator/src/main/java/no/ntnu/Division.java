1c21858e84b210695942ec22d27b290fa8c612b6
Simon Remøy <simon.remoy@gmail.com>
2010-12-10 14:16:33 +0100

    added two faults

--- calculator/src/main/java/no/ntnu/Division.java
+++ calculator/src/main/java/no/ntnu/Division.java
@@ -3,7 +3,7 @@ package no.ntnu;
 public class Division {
 	public static int divide(int numberA, int numberB) {
 		int counter = 0;
-		while(numberA >= numberB) {
+		while(numberA > numberB) {
 			counter++;
 			numberA = Subtraction.subtract(numberA, numberB);
 		}
--- calculator/src/main/java/no/ntnu/Factorial.java
+++ calculator/src/main/java/no/ntnu/Factorial.java
@@ -3,7 +3,7 @@ package no.ntnu;
 public class Factorial {
 	public static int factorial(int value) {
 		int result = 1;
-		for(int i = value; i > 0; i--) {
+		for(int i = value; i > 2; i--) {
 			result = Multiplication.multiply(result, i);
 		}
 		return result;
