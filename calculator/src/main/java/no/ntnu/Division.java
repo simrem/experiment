a29db967e2cdb47a83bb2e00bd0d3b034f7887e2
Simon Remøy <simon.remoy@gmail.com>
2010-12-10 15:02:47 +0100

    fixed first fault

--- calculator/src/main/java/no/ntnu/Division.java
+++ calculator/src/main/java/no/ntnu/Division.java
@@ -2,7 +2,7 @@ package no.ntnu;
 public class Division {
 	public static int divide(int numberA, int numberB) {
 		int counter = 0;
-		while(numberA >= numberB) {
+		while(numberA > numberB) {
 			counter++;
 			numberA = Subtraction.subtract(numberA, numberB);
 		}
